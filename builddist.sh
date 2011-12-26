261639789d08ea8d6d745eb3ab888fcbc639598a
Animesh Sharma <sharma.animesh@gmail.com>
2011-12-26 21:00:01 +0100

    4->5

--- builddist.sh
+++ builddist.sh
@@ -10,8 +10,6 @@ mkdir dist/atac
 mkdir dist/ESTmapper
 mkdir dist/snapper
 
-cp -p  dist/README.atac dist/atac/README
-
 cp -pr ../build configure.sh Make.include libutil libbio libsim4 sim4db sim4dbutils \
   dist/sim4db
 
@@ -21,7 +19,8 @@ cp -pr ../build configure.sh Make.include libutil libbio libmeryl meryl \
 cp -pr ../build configure.sh Make.include libutil libbio leaff \
   dist/leaff
 
-cp -pr ../build configure.sh Make.include libutil libbio libmeryl libkmer leaff meryl seatac atac-driver \
+#  fails in atac
+cp -pr ../build configure.sh Make.include libutil libbio libmeryl libkmer seatac atac atac-driver \
   dist/atac
 
 cp -pr ../build configure.sh Make.include libutil libbio ESTmapper leaff libkmer libmeryl libsim4 meryl seagen sim4db sim4dbutils \
@@ -30,7 +29,8 @@ cp -pr ../build configure.sh Make.include libutil libbio ESTmapper leaff libkmer
 cp -pr ../build configure.sh Make.include libutil libbio libkmer libmeryl libsim4 snapper \
   dist/snapper
 
-version=`date +%Y%m%d-%k%M | tr ' ' 0`
+
+version=`date +%Y%m%d-%k%M`
 
 cd dist
 tar -cf - sim4db    | bzip2 -9vc > sim4db-$version.tar.bz2
