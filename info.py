186215d6bfa8bf7a5cf27eb9e8b58973d2b96a83
Animesh Sharma <sharma.animesh@gmail.com>
2011-12-27 01:56:31 +0100

    3->4

--- info.py
+++ info.py
@@ -1,5 +1,29 @@
-"""
-Enhanced distutils with Fortran compilers support and more.
+"""\
+Core FFT routines
+==================
+
+ Standard FFTs
+
+   fft
+   ifft
+   fft2
+   ifft2
+   fftn
+   ifftn
+
+ Real FFTs
+
+   rfft
+   irfft
+   rfft2
+   irfft2
+   rfftn
+   irfftn
+
+ Hermite FFTs
+
+   hfft
+   ihfft
 """
 
-postpone_import = True
+depends = ['core']
