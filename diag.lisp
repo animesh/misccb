cb6e5b9617a4919fa4b10004dc2f9df65d457ef3
Animesh Sharma <sharma.animesh@gmail.com>
2011-12-29 00:29:27 +0100

    actr6 count.lisp example

--- diag.lisp
+++ diag.lisp
@@ -1,5 +1,9 @@
-(+ 1 1)
-
+;; (+ 1 1)
+(load "/media/DATA/actr6/load-act-r-6.lisp")
+;; load GUI ; cd /media/DATA/actr6/environment/GUI ; wish starter.tcl &
+(start-environment)
+(load "/media/DATA/actr6/tutorial/unit1/count.lisp")
+(run 1)
 
 
 
