3d2ce9586bda32bec1be1bb71d664c1db681f26c
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-08 20:10:54 +0100

    suggestions from http://www.perlmonks.org/?node_id=733923

--- overlap.pl
+++ overlap.pl
@@ -1,19 +1,11 @@
-#source http://stackoverflow.com/a/6026745/1137129
+#source http://www.perlmonks.org/?abspart=1;displaytype=displaycode;node_id=733924;part=1
 $str1=$ARGV[0];
 $str2=$ARGV[1];
 print overlap($str1,$str2),"\n";
 sub overlap{
-    my $c = 0;
-    my $ovl = "";
     my $s1 = shift;
     my $s2 = shift;
-    @s1=split(//,$s1);
-    @s2=split(//,$s1);
-    while ($s1[$c] and $s2[$c]){
-	if($s1[$c] eq $s2[$c]){
-		$ovl.=$s1[$c];
-        	$c++;
-	}
-    }
-    return ($ovl,$c);
+    my $ovl = $s1 ^ $s2;
+    $ovl =~ s/\0/X/g;
+    return ($ovl);
 }
