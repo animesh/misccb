88b184eb504a6b14da7bbcfc5d823929bf19c0ea
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-08 22:21:26 +0100

    Change 7->8

--- example.c
+++ example.c
@@ -1,19 +1,53 @@
 /* File : example.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int do_op(int a, int b, int (*op)(int,int)) {
-  return (*op)(a,b);
+/* Compute the greatest common divisor of positive integers */
+int gcd(int x, int y) {
+  int g;
+  g = y;
+  while (x > 0) {
+    g = x;
+    x = y % x;
+    y = g;
+  }
+  return g;
 }
 
-int add(int a, int b) {
-  return a+b;
+int gcdmain(int argc, char *argv[]) {
+  int x,y;
+  if (argc != 3) {
+    printf("usage: gcd x y\n");
+    return -1;
+  }
+  x = atoi(argv[1]);
+  y = atoi(argv[2]);
+  printf("gcd(%d,%d) = %d\n", x,y,gcd(x,y));
+  return 0;
 }
 
-int sub(int a, int b) {
-  return a-b;
+int count(char *bytes, int len, char c) {
+  int i;
+  int count = 0;
+  for (i = 0; i < len; i++) {
+    if (bytes[i] == c) count++;
+  }
+  return count;
 }
 
-int mul(int a, int b) {
-  return a*b;
+void capitalize(char *str, int len) {
+  int i;
+  for (i = 0; i < len; i++) {
+    str[i] = (char)toupper(str[i]);
+  }
 }
 
-int (*funcvar)(int,int) = add;
+void circle(double x, double y) {
+  double a = x*x + y*y;
+  if (a > 1.0) {
+    printf("Bad points %g, %g\n", x,y);
+  } else {
+    printf("Good points %g, %g\n", x,y);
+  }
+}
