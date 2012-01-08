cf4cc89008b273a631ceb6e5cf29d46a507e4c0a
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-08 20:15:04 +0100

    Code from wikibooks for LCS

--- overlap.pl
+++ overlap.pl
@@ -1,11 +1,31 @@
-#source http://www.perlmonks.org/?abspart=1;displaytype=displaycode;node_id=733924;part=1
+#source http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Perl
 $str1=$ARGV[0];
 $str2=$ARGV[1];
 print overlap($str1,$str2),"\n";
 sub overlap{
-    my $s1 = shift;
-    my $s2 = shift;
-    my $ovl = $s1 ^ $s2;
-    $ovl =~ s/\0/X/g;
-    return ($ovl);
+  my ($str1, $str2) = @_; 
+  my $l_length = 0; # length of longest common substring
+  my $len1 = length $str1; 
+  my $len2 = length $str2; 
+  my @char1 = (undef, split(//, $str1)); # $str1 as array of chars, indexed from 1
+  my @char2 = (undef, split(//, $str2)); # $str2 as array of chars, indexed from 1
+  my @lc_suffix; # "longest common suffix" table
+  my @substrings; # list of common substrings of length $l_length
+  for my $n1 ( 1 .. $len1 ) { 
+    for my $n2 ( 1 .. $len2 ) { 
+      if ($char1[$n1] eq $char2[$n2]) {
+        $lc_suffix[$n1-1][$n2-1] ||= 0;
+        $lc_suffix[$n1][$n2] = $lc_suffix[$n1-1][$n2-1] + 1;
+        if ($lc_suffix[$n1][$n2] > $l_length) {
+          $l_length = $lc_suffix[$n1][$n2];
+          @substrings = ();
+        }
+        if ($lc_suffix[$n1][$n2] == $l_length) {
+          push @substrings, substr($str1, ($n1-$l_length), $l_length);
+        }
+      }
+    }
+  }   
+ 
+  return @substrings;
 }
