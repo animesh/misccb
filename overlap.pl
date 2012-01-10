03dbacec9554e8af77618bb6b248d7517adb741b
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-10 17:37:58 +0100

    generate and break DNA strings

--- overlap.pl
+++ overlap.pl
@@ -1,7 +1,19 @@
 #source http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Perl
 $str1=$ARGV[0];
 $str2=$ARGV[1];
-print overlap($str1,$str2),"\n";
+@reads=<>;
+for($c1=0;$c1<=$#reads;$c1++){
+	$str1=$reads[$c1];
+	$str1=~s/\s+|\n//g;
+	for($c2=$c1+1;$c2<=$#reads;$c2++){
+		$str2=$reads[$c2];
+		$str2=~s/\s+|\n//g;
+		if($str2 ne $str1 and $str1 ne "" and $str2 ne ""){
+			print overlap($str1,$str2),"\n";
+		}
+	}
+}
+#print overlap($str1,$str2),"\n";
 sub overlap{
   my ($str1, $str2) = @_; 
   my $l_length = 0; # length of longest common substring
@@ -29,3 +41,11 @@ sub overlap{
  
   return @substrings;
 }
+__END__
+ 2076  perl -e '@b=qw/A T G C/;print "while($l<1000){print @b[int(rand(4))];$l++;}'
+ 2073  perl -ne 'if ($p) { for($c=0;$c<length;$c++){print substr($_,$c,10);print "\n"}; $p = 0 } $p++ if />/' rangen.txt  > rangen.txt.kmer
+ 2075  time perl overlap.pl rangen.txt.kmer
+
+perl -e '@b=qw/A T G C/;while($l<100){print @b[int(rand(4))];$l++;}' > rangen.txt
+perl -ne 'for($c=0;$c<length;$c++){print substr($_,$c,10);print "\n"};' rangen.txt > rangen.txt.kmer 
+
--- simtop.py
+++ simtop.py
@@ -1,18 +1,57 @@
-import nest
+#http://www.neurdon.com/2011/03/06/spiking-neural-networks-in-python-part-1/
+from __future__ import division
 import numpy as np
-#from NeuroTools import signals
-nest.ResetKernel()
-nest.SetKernelStatus({"resolution":.01})
-nest.SetKernelStatus({"overwrite_files":True})
-hh_neuron=nest.Create("hh_cond_exp_traub",n=1)
-nest.SetStatus(hhneuron,{"I_e": 2000.})
-voltmeter=nest.Create("voltmeter")
-nest.SetStatus(voltmeter,{'to_file':True,'interval':.1})
-nest.Connect(voltmeter,hh_neuron)
-nest.Simulate(200.)
-vm=np.loadtxt('voltmeter-4-0.dat')
-plot(vm[:,1],vm[:,2])
-gca().set_xlabel("Time(msec)")
-gca().set_ylabel("Vm(mV)")
-draw()
+import pylab as py
 
+## setup parameters and state variables
+N    = 10                      # number of neurons
+T    = 200                     # total time to simulate (msec)
+dt   = 0.125                   # simulation time step (msec)
+time = np.arange(0, T+dt, dt)  # time array
+
+
+## LIF properties
+Vm      = np.zeros([N,len(time)])  # potential (V) trace over time
+tau_m   = 10                       # time constant (msec)
+tau_ref = 4                        # refractory period (msec)
+tau_psc = 5                        # post synaptic current filter time constant
+Vth     = 1                        # spike threshold (V)
+
+## Currents
+I    = np.zeros((N,len(time)))
+Iext = np.zeros(N) # externally applied stimulus
+Iext[0] = 1.5
+
+## Synapse weight matrix
+# equally weighted ring connectivity
+synapses = np.eye(N)
+synapses = np.roll(synapses, -1, 1)
+
+# randomly weighted full connectivity
+#synapses = np.random.rand(N,N)*0.3
+
+## Synapse current model
+def Isyn(t):
+    '''t is an array of times since each neuron's last spike event'''
+    t[np.nonzero(t < 0)] = 0
+    return t*np.exp(-t/tau_psc)
+last_spike = np.zeros(N) - tau_ref
+
+## Simulate network
+raster = np.zeros([N,len(time)])*np.nan
+for i, t in enumerate(time[1:],1):
+    active = np.nonzero(t > last_spike + tau_ref)
+    Vm[active,i] = Vm[active,i-1] + (-Vm[active,i-1] + I[active,i-1]) / tau_m * dt
+
+    spiked = np.nonzero(Vm[:,i] > Vth)
+    last_spike[spiked] = t
+    raster[spiked,i] = spiked[0]+1
+    I[:,i] = Iext + synapses.dot(Isyn(t - last_spike))
+
+## plot membrane potential trace
+py.plot(time, np.transpose(raster), 'b.')
+py.title('Recurrent Network Example')
+py.ylabel('Neuron')
+py.xlabel('Time (msec)')
+py.ylim([0.75,N+0.25])
+py.show()
