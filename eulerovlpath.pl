637bb34900f553a78b6a830865ba7b14d1278d93
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-10 18:58:15 +0100

    overlap graph and merge

--- eulerovlpath.pl
+++ eulerovlpath.pl
@@ -1,5 +1,3 @@
-#source http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Perl
-
 use strict;
 use warnings;
 my @reads=<>;
@@ -28,6 +26,7 @@ for(my $c1=0;$c1<=$#reads;$c1++){
 	}
 }
 
+#source http://en.wikibooks.org/wiki/Algorithm_implementation/Strings/Longest_common_substring#Perl
 sub overlap{
   my ($str1, $str2) = @_; 
   my $l_length = 0; # length of longest common substring
@@ -56,6 +55,59 @@ sub overlap{
   return @substrings;
 }
 
+#source http://stackoverflow.com/questions/4031325/finding-eulerian-path-in-perl
+
+sub eulerPath {
+
+    my %graph = @_;
+
+    # count the number of vertices with odd degree
+    my @odd = ();
+    foreach my $vert ( sort keys %graph ) {
+        my @edg = @{ $graph{$vert} };
+
+        my $size = scalar(@edg);
+        if ( $size % 2 != 0 ) {
+            push @odd, $vert;
+        }
+    }
+
+    push @odd, ( keys %graph )[0];
+
+    if ( scalar(@odd) > 3 ) {
+        return "None";
+
+    }
+
+    my @stack = ( $odd[0] );
+    my @path  = ();
+
+    while (@stack) {
+        my $v = $stack[-1];
+        #suggestion http://stackoverflow.com/a/4031608
+        if ( @{$graph{$v}} ) {
+                my $u = ( @{ $graph{$v} } )[0];
+                push @stack, $u;
+              # Find index of vertice v in graph{$u}
+
+            my @graphu = @{ $graph{$u} };  # This is line 54.
+            my ($index) = grep $graphu[$_] eq $v, 0 .. $#graphu;
+            #suggestion http://stackoverflow.com/a/4031608
+            splice @{ $graph{$u} }, $index, 1;
+            splice @{ $graph{$v} }, 0, 1;
+
+        }
+        else {
+
+            push @path, pop(@stack);
+        }
+
+    }
+
+    print Dumper \@path;
+
+    return @path;
+}
 
 
 __END__
