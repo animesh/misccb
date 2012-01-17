01a05d042abcc34db0ba6ea272c3febd0406ef0a
Animesh Sharma <sharma.animesh@gmail.com>
2012-01-17 16:25:32 +0100

    structure

--- save_wv_crs.r
+++ save_wv_crs.r
@@ -4,15 +4,12 @@ wv=read.csv('data/wvWP.csv',header=F)
 
 jpeg('result/crs.jpg')
 plot(crs)
+
 jpeg('result/save.jpg')
 plot(save)
+
 jpeg('result/wv.jpg')
 plot(wv)
-
-jpeg('result/regsaveV6crsV7.jpg')
-reg=glm(save$V6~crs$V7)
-plot(save$V6~crs$V7)
-abline(reg)
 
 cor.test(crs$V5,crs$V6)
 cor.test(save$V5,save$V6)
@@ -39,5 +36,9 @@ summary(glm(save$V5~save$V6))
 summary(glm(crs$V6~crs$V5))
 summary(glm(save$V6~save$V5))
 summary(glm(wv$V6~wv$V5))
+
+jpeg('result/regsaveV6crsV7.jpg')
+plot(save$V6~crs$V7)
+abline(glm(save$V6~crs$V7))
 
 
