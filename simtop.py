7fd1ac9551d58674455b359a072e8683de00b079
Animesh Sharma <sharma.animesh@gmail.com>
2012-02-03 20:09:36 +0100

    generator added, pois, gex, gin

--- simtop.py
+++ simtop.py
@@ -1,5 +1,6 @@
 import nest
 import nest.topology as topo
+import numpy as np
 import math
 import pylab
 import time
@@ -7,14 +8,14 @@ foo='simtop.'+str(time.time())
 nest.ResetKernel()
 
 sim_int=5.0
-sim_time=100.0
+sim_time=500.0
 
 excitatory_dict = {
     "rows": 30,
     "columns": 30,
     "extent": [2.0, 2.0],
     "center": [0.0, 0.0],
-    "elements": "iaf_neuron",
+    "elements": "iaf_cond_alpha",
     "edge_wrap": True
     }
 
@@ -43,7 +44,7 @@ mix = topo.CreateLayer(mix_dict)
 exc_par = {
     "connection_type": "convergent",
     "mask": {"circular": {"radius": 1.0}},
-    "weights": 4.0,
+    "weights": 40.0,
     "delays": 1.5,
     "kernel": {"gaussian": {"sigma": 0.3,"p_center": 1.3}},
     "allow_autapses": True,
@@ -54,7 +55,7 @@ exc_par = {
 inh_par = {
     "connection_type": "convergent",
     "mask": {"circular": {"radius": 0.5}},
-    "weights": -1.0,
+    "weights": -10.0,
     "delays": 1.5,
     "kernel": {"gaussian": {"sigma": 0.3, "p_center": 1.3}},
     "allow_autapses": True,
@@ -65,7 +66,7 @@ inh_par = {
 mix_par = {
     "connection_type": "convergent",
     "mask": {"circular": {"radius": 0.5}},
-    "weights": -2.0, 
+    "weights": -20.0, 
     "delays": 1.0,
     "kernel": {"gaussian": {"sigma": 0.3, "p_center": 1.3}},
     "allow_autapses": True,
@@ -77,51 +78,68 @@ topo.ConnectLayers(exc,exc,exc_par)
 topo.ConnectLayers(exc,inh,exc_par)
 topo.ConnectLayers(inh,inh,inh_par)
 topo.ConnectLayers(inh,exc,inh_par)
-topo.ConnectLayers(exc,mix,mix_par)
-topo.ConnectLayers(mix,inh,mix_par)
+topo.ConnectLayers(exc,mix,exc_par)
+topo.ConnectLayers(mix,inh,exc_par)
+topo.ConnectLayers(mix,exc,inh_par)
 
 nest.PrintNetwork(depth=2)
+
+pois = nest.Create("poisson_generator")
+gex = nest.Create('spike_generator',
+                  params = {'spike_times': np.array([10.0, 20.0, 50.0])})
+gin = nest.Create('spike_generator',
+                  params = {'spike_times': np.array([15.0, 25.0, 55.0])})
+
+nest.DivergentConnect(gex, nest.GetNodes(exc)[0])
+nest.DivergentConnect(gin, nest.GetNodes(inh)[0])
+nest.DivergentConnect(pois, nest.GetNodes(mix)[0])
 
 mixpos = zip(*[topo.GetPosition([n]) for n in nest.GetLeaves(mix)[0]])[0]
 #pylab.plot(mixpos[0], mixpos[1], 'x')
 #pylab.show()
 
-nest.CopyModel('multimeter', 'RecordingNode',
+#nest.CopyModel('multimeter', 'RecordingNode',
+#               params = {'withtime': True, 
+#                         'withgid': True,
+#                         'to_file': True,
+#                         'label': foo,
+#                         'interval': sim_int,
+#                         'record_from': ['V_m', 'g_ex', 'g_in'],
+#                         'withpath'   : True })
+
+mixmm=nest.Create('multimeter',
                params = {'withtime': True, 
                          'withgid': True,
                          'to_file': True,
-                         'label': foo,
+                         'label': foo+'mixmm',
                          'interval': sim_int,
                          'record_from': ['V_m', 'g_ex', 'g_in'],
                          'withpath'   : True })
 
 #mm=nest.Create('RecordingNode')
 #nest.DivergentConnect(mm, nest.GetNodes(exc)[0])
-#nest.DivergentConnect(mm, nest.GetNodes(inh)[0])
+tgts = [nd for nd in nest.GetLeaves(mix)[0] 
+        if nest.GetStatus([nd], 'model')[0]==model]
+nest.DivergentConnect(mixmm,tgts)
 
-recorders = {}
-for name, loc, population, model in [
-    ('mix',1,mix,'mix'),
-    ('inh',2,inh , 'inh'),
-    ('exc', 3, exc, 'exc')
-    ]:
+#recorders = {}
+#for name, loc, population, model in [
+    #('mix',1,mix,'mix'),
+    #('inh',2,inh , 'inh'),
+    #('exc', 3, exc, 'exc')
+    #]:
     #nest.DivergentConnect(mm,name)
-    recorders[name] = (nest.Create('RecordingNode'), loc)
-    tgts = [nd for nd in nest.GetLeaves(population)[0] 
-            if nest.GetStatus([nd], 'model')[0]==model]
-    nest.DivergentConnect(recorders[name][0], tgts) 
+    #recorders[name] = (nest.Create('RecordingNode'), loc)
+    #tgts = [nd for nd in nest.GetLeaves(population)[0] 
+    #        if nest.GetStatus([nd], 'model')[0]==model]
+    #nest.DivergentConnect(recorders[name][0], tgts) 
 
 nest.SetStatus([0],{'print_time': True})
-
-pois = nest.Create("poisson_generator")
-nest.DivergentConnect(pois, nest.GetNodes(exc)[0])
-nest.DivergentConnect(pois, nest.GetNodes(inh)[0])
-
 
 nest.Simulate(sim_int)
 
 #for t in  pylab.arange(sim_int, sim_time, sim_int):
-for t in  range(0, int(sim_time)):
+for t in  range(0, 0):
     print t
     nest.Simulate(sim_int)
     #pylab.clf()
