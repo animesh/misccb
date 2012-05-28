633a60fd7cb65dd5e8bf78f616d1f8fedb371140
Animesh Sharma <sharma.animesh@gmail.com>
2012-05-28 17:29:48 +0530

    parse MS and mRNA

---
+++ mrna-prot-con.pl
@@ -0,0 +1,46 @@
+use strict;
+
+my $f1 = shift @ARGV;
+my $f2 = shift @ARGV;
+my @tmp;
+my @name;
+my %pg;
+my %mrna;
+
+open (F1, $f1) || die "can't open \"$f1\": $!";
+while (my $line = <F1>) {
+	@tmp=split(/\t/,$line);
+        if ($tmp[0] =~ /^[0-9]/){
+ 		@name=split(/\;/,$tmp[12]);
+ 		foreach (@name) { $pg{$_}=$tmp[48];} 	
+		#print "$name[1]\t$tmp[48]\n";
+        }
+}
+close F1;
+
+open (F2, $f2) || die "can't open \"$f2\": $!";
+while (my $line = <F2>) {
+	$line=~s/\"//g;
+        if ($line =~ /^[0-9]/){
+                @tmp=split(/\|/,$line);
+                $mrna{$tmp[1]}=$tmp[9];
+		#print "$tmp[1]\t$tmp[9]\n";
+        }
+}
+close F2;
+
+foreach my $pgn (keys %pg){
+	foreach my $mn (keys %mrna){
+		if($pgn eq $mn){
+			print "$pgn\t$mn\t$pg{$pgn}\t$mrna{$mn}\n"
+		}
+	}
+}
+
+__END__
+for(my $cnt=0;$cnt<=$#tmp;$cnt++){
+            my $val=$tmp[$cnt];
+            #print "$val\n";
+}       
+
+
--- parse-match-atac.pl
+++ parse-match-atac.pl
@@ -4,12 +4,12 @@ while(<>){
 	@n=split(/vs/,$t[0]);
 	push(@ns1,$n[0]);
 	push(@ns2,$n[1]);
-	$m{"$n[0]-$n[1]"}=join(' - ',@t[1..$#t]);
+	$m{"$n[0]-$n[1]"}="$t[1]-$t[2]-$t[7]";
 }
 %seen = (); @ns1 = grep { ! $seen{ $_ }++ } @ns1;
 %seen = (); @ns2 = grep { ! $seen{ $_ }++ } @ns2;
 for($c1=0;$c1<=$#ns1;$c1++){
-	if($c1==0){print "GI , ";for($c=1;$c<=$#ns1;$c++){print "$ns1[$c] , ";}print "\n";}
+	if($c1==0){print "GI , ";for($c=0;$c<=$#ns2;$c++){print "$ns2[$c] , ";}print "\n";}
 	print "$ns1[$c1] , ";
 	for($c2=0;$c2<=$#ns2;$c2++){
 		if($m{"$ns1[$c1]-$ns2[$c2]"} ne ""){
