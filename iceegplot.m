e30578f02624028176356deaf8631ff0b8e726c3
Ani <sharma.animesh@gmail.com>
2012-08-15 13:43:41 +0530

    symbolic

--- iceegplot.m
+++ iceegplot.m
@@ -1,8 +1,17 @@
 %% read data
 d=load('C:\Users\Amithabh\Desktop\ani\iceeg\Sample.mat')
-%plot(d)
-show d
+[r c]=size(d.d)
+plot(d.d(r,:))
 
 %% check
+plot(d.d(1:50,:))
+autocorr(d.d(r,:)')
 
-x=1:100
+%% symbolic test
+
+% source http://blogs.mathworks.com/loren/2012/07/27/using-symbolic-equations-and-symbolic-functions-in-matlab/?s_eid=PSM_1986
+
+besselODE = 't^2*D2y+t*Dy+(t^2-n^2)*y';
+f = dsolve(besselODE,'y(1)=1','y(2)=n','t');
+pretty(f)
+
