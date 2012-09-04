8f4d703c6df6a2b053338d05ffe724e3931e4153
Ani <sharma.animesh@gmail.com>
2012-09-04 18:46:48 +0530

    back

--- fac.pl
+++ fac.pl
@@ -1,10 +1,10 @@
-$sn=rec(@ARGV[0]);
+$sn = rec(@ARGV[0]);
 print "$sn\n";
-sub rec{
-print "$n\n";
-$n=shift;
-if($n<=1){exit;}
-else{$p=$n*rec($n-1);}
-return $p;
+sub rec
+{
+    print "$n\n";
+    $n = shift;
+    if($n <= 1) {exit;}
+    else{$p = $n * rec($n - 1);}
+    return $p;
 }
- 
