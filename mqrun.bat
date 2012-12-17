20d82000570728007846f0eef6c93e7f3c18a97a
Ani <sharma.animesh@gmail.com>
2012-12-17 15:54:42 +0100

    create xml for mascot

--- mqrun.bat
+++ mqrun.bat
@@ -1,28 +1,34 @@
-DIR /B M:\RAW\KA\*.raw > tempfile.txt
+DIR /B M:\RAW\KA\P\12*.raw > tempfile.txt
 
-
-FOR /F "tokens=1,2 delims=." %%i in (tempfile.txt) do @echo %%i
+FOR /F "tokens=1,2 delims=." %%i in (tempfile.txt) do call :Change %%i
 
 :: FOR %%A IN (DEL) DO %%A tempfile.txt
 
 :: C:\Users\animeshs\SkyDrive\MaxQuant\bin\MaxQuantCmd.exe C:\Users\animeshs\Desktop\check.xml 4
 
-:: Source http://www.robvanderwoude.com/for.php 
 
-    
-  
-  
-if exist M:\RAW\KA\KAParCpN.xml del M:\RAW\KA\KAParCpN.xml
-for /f "tokens=2 delims= " %%a in (M:\RAW\KA\KAParCp.xml) do call :Change "%%a"
-exit /b
+
 
 :Change
-set Text=%~1
-if "%Text%"=="Karpas_4%_2_101212_121213103116" (
-(@echo %%i)>> M:\RAW\KA\KAParCpN.xml
-) else (
-(echo %Text%)>> M:\RAW\KA\KAParCpN.xml
-)
-exit /b
+setlocal enabledelayedexpansion
+set FileN=%~1
+set INTEXTFILE=M:\RAW\KA\P\KAParCp.xml
+set OUTTEXTFILE=M:\RAW\KA\P\%FileN%.xml
+set SEARCHTEXT=TestFile
+set REPLACETEXT=%FileN%
+set OUTPUTLINE=
 
-:: Source https://irfanview-forum.de/showthread.php?t=3263 
+if exist %OUTTEXTFILE% del %OUTTEXTFILE%
+for /f "tokens=1,* delims=¶" %%A in ( '"type %INTEXTFILE%"') do (
+SET string=%%A
+SET modified=!string:%SEARCHTEXT%=%REPLACETEXT%!
+
+echo !modified! >> %OUTTEXTFILE%
+)
+
+
+:: Source 
+:: https://irfanview-forum.de/showthread.php?t=3263 
+:: http://stackoverflow.com/questions/5273937/how-to-replace-substrings-in-windows-batch-file
+:: http://www.pcreview.co.uk/forums/delims-t1466398.html
+:: http://www.robvanderwoude.com/for.php 
