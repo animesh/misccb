a1b8c9e5f8afc134ee4e71a53ef5e82d27a010b6
Ani <sharma.animesh@gmail.com>
2013-01-12 18:27:38 +0100

    run maxquant in batch and copy result folder

--- mqrun.bat
+++ mqrun.bat
@@ -1,7 +1,7 @@
 set MAXQUANTDIR=C:\Users\animeshs\SkyDrive\MaxQuant
-set DATADIR=M:\RAW\P
-set PREFIXRAW=npq
-set PARAMFILE=mqpar.xml
+set DATADIR=X:\Qexactive
+set PREFIXRAW=Karpas
+set PARAMFILE=testpar.xml
 set SEARCHTEXT=TestFile
 
 DIR /B %DATADIR%\%PREFIXRAW%*.raw > %DATADIR%\tempfile.txt
@@ -9,9 +9,10 @@ DIR /B %DATADIR%\%PREFIXRAW%*.raw > %DATADIR%\tempfile.txt
 FOR /F "eol=  tokens=1,2 delims=." %%i in (%DATADIR%\tempfile.txt) do  ( 
 	if not %%i ==   "" (
 		call :Change %%i
+		if exist proc rmdir /S /Q proc
 		%MAXQUANTDIR%\bin\MaxQuantCmd.exe %DATADIR%\%%i.xml 4
-		if exist %DATADIR%\%%i.txt del %DATADIR%\%%i.txt
-		copy %DATADIR%\combined\txt\proteinGroups.txt %DATADIR%\%%i.txt
+		if exist %DATADIR%\%%iRes rmdir /S /Q %DATADIR%\%%iRes
+		echo D| xcopy  /E /Y /Q %DATADIR%\combined\txt %DATADIR%\%%iRes
 	)
 )
 
@@ -43,3 +44,4 @@ GOTO :Source
 :: http://www.pcreview.co.uk/forums/delims-t1466398.html
 :: http://www.robvanderwoude.com/for.php
 :: http://stackoverflow.com/questions/3713601/subroutines-in-batch-files
+:: update at sharma.animesh@gmail.com :)
