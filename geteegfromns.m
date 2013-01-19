128d1dd2f569a08998176d98f7c035b397790671
U-WIN-NTNU-NO\animeshs <animeshs@DMED5714.klientmaskiner.medisin.ntnu.no>
2013-01-19 21:43:57 +0100

    power spectra beginning

--- geteegfromns.m
+++ geteegfromns.m
@@ -44,7 +44,7 @@ fclose(ns);
 
 %% analyse signal file
 
-ms=load('eegsig.txt');
+ms=load('C:\Users\animeshs\SkyDrive\eegsig.txt');
 
 for i = 1:size(ms,1)
     periodogram(ms(i,:))
@@ -52,11 +52,44 @@ for i = 1:size(ms,1)
 end
 
 
+%% signal linearized
+msl=reshape(ms',1,[]);
+periodogram(msl);
+cca_granger_regress(msl,1)
+
+
+%% fft
+
+msl=reshape(ms',1,[]);
+plot(msl)
+
+mslfft=fft(msl);
+amslfft=abs(mslfft);
+pmslfft=angle(mslfft);
+plot(amslfft(1:length(msl)/2)/length(msl)/2)
+
+i=length(mslfft)/2-1;
+while i>0
+[v,i]=max(amslfft(2:i));
+freq=(i*size(ms,2))/(length(mslfft)/2)
+end
+
+
+%% power
+
+msl=reshape(ms',1,[]);
+freq = 4000;
+win_PSD = 5*freq;
+noverlap_PSD = [];
+nFFT = 2^10;
+[PSD_RSM, f] = pwelch(msl, win_PSD, noverlap_PSD, nFFT, freq)
 
 
 
-
+    
 %% source 
 http://www.mathworks.se/help/instrument/reading-and-writing-data-over-the-bluetooth-interface.html
 http://stackoverflow.com/questions/3115833/real-time-plot-in-matlab
+http://www.nbtwiki.net/doku.php?id=tutorial:power_spectra_wavelet_analysis_and_coherence#.UPW_kEEcLt1
+
 
