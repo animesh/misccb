ddc53c1841ea0088628a9fb1fdefa8647c7b45a7
Ani <sharma.animesh@gmail.com>
2013-02-19 12:01:51 +0100

    regression

--- peparea.m
+++ peparea.m
@@ -1,7 +1,7 @@
 %% read csv
 
 pep=csvread('X:\Results\Sissel\HeK50\peparea.csv');
-pepval=csvread('X:\Results\Sissel\HeK50\pep12.csv');
+%pepval=csvread('X:\Results\Sissel\HeK50\pep12.csv');
 
 %% peptide area against concentration
 
@@ -16,3 +16,16 @@ boxplot(pep)
 plot(pep(1,:),pep(:,:),'r.');
 corrcoef(sum(pep),pep(1,:)),
 plot(sum(pep(2:end,:)),pep(1,:),'b.')
+
+%% regression
+d=100
+m=2
+c=10
+x=randi(d*d,d,1)
+y=m*x+c
+reg=[x ones(size(x))]\y
+
+
+
+%% source
+http://stackoverflow.com/questions/9315666/matlab-linear-regression
