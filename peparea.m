0a7da5a4fa86c5c1416c812676cb0ab44fad721d
Ani <sharma.animesh@gmail.com>
2013-03-18 16:07:05 +0100

    WTH retention time result analysis

--- peparea.m
+++ peparea.m
@@ -1,31 +1,56 @@
 %% read csv
 
-pep=csvread('X:\Results\Sissel\HeK50\sequest\peparea.csv');
-pep=csvread('X:\Results\Sissel\HeK50\mascot\peparea.csv');
+pep=csvread('X:\Elite\LARS\2013\mars\tobias\reverse.csv');
+pepval=csvread('M:\Results\Sissel\HeK50\mascot\peparea.csv');
 %pepval=csvread('X:\Results\Sissel\HeK50\sequest\pep12.csv');
+%rt=csvread('X:\Qexactive\WTH6\WTHRT9RepMHpep.csv',1,6);
+rt=xlsread('X:\Qexactive\WTH6\Multiconsensus from 9 ReportsMHpep.xlsx');
 
 %% peptide area against concentration
 
-plot(pepval(1,:),pepval(2:44382,:),'r.')
+plot(pepval(1,:),pepval(2:end,:),'r.')
 pvc=corr(pepval);
 plot(pvc)
-plot(pepval)
+plot(log(pep(:,1)),log(pep(:,2)),'b.')
+qqplot((pep(:,1)),(pep(:,2)))
+qqplot((pep(:,1)))
+corr((pep(:,1)),(pep(:,2)))
+ksdensity(log(pep(:,1)))
 
 %% pep raw
-
+rtv=[2,2,2,4,4,4,6,6,6]
+sum(rt(:,48)>=0)
+boxplot(rt(:,7:15))
+boxplot([rt(:,48),rt(:,66),rt(:,84),rt(:,49),rt(:,67),rt(:,85),rt(:,50),rt(:,68),rt(:,86)])
+plot(rtv,[ sum(rt(:,48)>=0), sum(rt(:,66)>=0), sum(rt(:,84)>=0), sum(rt(:,49)>=0), sum(rt(:,67)>=0), sum(rt(:,85)>=0), sum(rt(:,50)>=0), sum(rt(:,68)>=0), sum(rt(:,86)>=0)],'r.')
+plot(rt(:,48),rt(:,50),'g.')
+scatter3(rt(:,48),rt(:,49),rt(:,50))
+scatter3(rt(:,7),rt(:,8),rt(:,9))
+hist(rt(:,49))
+corrcoef(rt(:,7:15))
+sum(rt(:,7:15))
 boxplot(pep)
 stem(pep(2:end,:))
 plot(pep(1,:),pep(:,:),'r.');
 corrcoef(sum(pep),pep(1,:)),
 plot(sum(pep(2:end,:)),pep(1,:),'b.')
 
+
 %% error bar plot
 
 plot(pep(1,:), sum(pep(2:end,:))/length(pep),'ro')
 hold
+plot(pep(1,:), std(pep(2:end,:)),'b*')
+hold
+
+hist(pep((pep(2:end,1))>10000))
+
+hold
 errorbar(pep(1,:),mean(pep(2:end,:)),std(pep(2:end,:)),'<k--');
 hold
 
+hist(log(pep(2:end,1)))
+hist(pep(pep(2:end,1)>10000000,1),[200])
 
 
 %% regression
