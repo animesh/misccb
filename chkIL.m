f8c46778c7e9693e3bec596b72ab0db45607d78b
Ani <sharma.animesh@gmail.com>
2013-06-20 17:56:44 +0200

    PEP log transform for IPA, thermophoresis biphasic simulation, check Elite exclusion list, imgae pixel distance

--- chkIL.m
+++ chkIL.m
@@ -6,25 +6,29 @@ EL1=xlsread('X:\Qexactive\130529_Incremental\run1\EL1.xls')
 EL2=xlsread('X:\Qexactive\130529_Incremental\run1\EL2.xls')
 EL3=xlsread('X:\Qexactive\130529_Incremental\run1\EL3.xls')
 
+
 %% SIM stuff
 
 a=xlsread('X:\Elite\Alexey\Test_Incl_Excl\Multiconsensus from 3 ReportsSImod.xlsx');
+a=xlsread('X:\Elite\Alexey\HCD\130617_1SegGenMSSI.xlsx')
 EL=xlsread('X:\Elite\Alexey\Test_Incl_Excl\List.xlsx');
 ELMI=xlsread('X:\Elite\Alexey\Test_Incl_Excl\peptides.txt.pepmonoisomass.xlsx');
 
 
 %% extract vals and compare
 
-mzt=a(a(:,14)==0,9);
+mzt=-1;
 mze=EL(:,1);
 cntax=0;
 ppm=10;
 detect=zeros(size(mze,1),size(a,1));
 for i=1:size(mze,1)
     for j=1:size(a,1)
-        if(a(j,9)<=(mze(i)+mze(i)*(ppm/10e6)) && a(j,9)>=(mze(i)-mze(i)*(ppm/10e6)) && a(j,14)==-1)
+        if(a(j,4)<=(mze(i)+mze(i)*(ppm/10e6)) && a(j,4)>=(mze(i)-mze(i)*(ppm/10e6)))
+        %if(a(j,9)<=(mze(i)+mze(i)*(ppm/10e6)) && a(j,9)>=(mze(i)-mze(i)*(ppm/10e6)) && a(j,14)==mzt)
+        %if(a(j,9)>(mze(i)+mze(i)*(ppm/10e6)) || a(j,9)<(mze(i)-mze(i)*(ppm/10e6)) && a(j,14)==mzt)
            cntax=cntax+1;
-           detect(i,j)=mze(i)-a(j,9);
+           detect(i,j)=mze(i)-a(j,4);
         end
     end
 end
--- scorecomp.pl
+++ scorecomp.pl
@@ -1,25 +1,66 @@
+use strict;
+use Text::ParseWords;
 open(F1,$ARGV[0]);
-$s1=$ARGV[1];
-$s2=$ARGV[2];
-$s3=$ARGV[3];
-$s4=$ARGV[4];
+my $s1=$ARGV[1];
+my $s2=$ARGV[2];
+my $s3=$ARGV[3];
+my $s4=$ARGV[4];
+my %val1;
+my %tm1;
+my %val2;
+my %tm2;
+my %tm;
+my $lc;
 
-while($l=<F1>){
-	@tmp1=split(/,/,$l);
-	if($tmp1[$s1] and $tmp1[$s3]){
-		$match{$tmp1[0]}.="$tmp1[$s1]\t$tmp1[$s2]\t$tmp1[$s3]\t$tmp1[$s4],";
-		
+while(my $l=<F1>){
+	$lc++;
+	if($lc>1){
+		chomp $l;
+		$l =~ s/\r//g;
+		my @tmp1=split(/,/,$l);
+		#my @tmp1=parse_line(',',0,$l); # take about 10 times more processing!
+		my @tmp2=split(/;/,$tmp1[$s1]);
+		for(my $c1=0;$c1<=$#tmp2;$c1++){
+			if($tmp1[$s3] eq "130603_Berit_7_AID_YFP.raw" or $tmp1[$s3] eq "130603_Berit_8_AID_YFP.raw" or $tmp1[$s3] eq "130603_Berit_9_AID_YFP.raw"){
+				$val1{$tmp2[$c1]}+=log($tmp1[$s2]);
+				$tm1{$tmp2[$c1]}++;
+			}
+			if($tmp1[$s3] eq "130603_Berit_10_AID_YFP.raw" or $tmp1[$s3] eq "130603_Berit_11_AID_YFP.raw" or $tmp1[$s3] eq "130603_Berit_12_AID_YFP.raw"){
+				$val2{$tmp2[$c1]}+=log($tmp1[$s2]);
+				$tm2{$tmp2[$c1]}++;
+			}
+			$tm{$tmp2[$c1]}++;
+		}
 	}
-	else{next;}
- 		
 }
 
-foreach (keys %match){if($match{$_}){print "$_ , $match{$_}\n"}}
+foreach (keys %tm){print "$_,",$val2{$_}-$val1{$_},",$val1{$_},$val2{$_},$tm1{$_},$tm2{$_},$tm{$_}\n"}
 
-system("perl -l /cygdrive/C/Users/animeshs/SkyDrive/Mayu/lib/ /cygdrive/C/Users/animeshs/SkyDrive/Mayu/Mayu.pl")
+
+#system("perl -l /cygdrive/C/Users/animeshs/SkyDrive/Mayu/lib/ /cygdrive/C/Users/animeshs/SkyDrive/Mayu/Mayu.pl")
+
 
 
 __END__
 
-perl scorecomp.pl /cygdrive/X/Qexactive/Berit_Sissel/130521_Berit_Aid_YFP/0130603_Berit_tek_rep_1_12/score-mix.csv 7 11 15 19 | less
+
+time perl scorecomp.pl /cygdrive/X/Qexactive/Berit_Sissel/130521_Berit_Aid_YFP/0130603_Berit_tek_rep_1_12/Pep789101112WG.csv 8 16 32 > scoremix.csv
+ 
+
+perl scorecomp.pl /cygdrive/X/Qexactive/Berit_Sissel/130521_Berit_Aid_YFP/0130603_Berit_tek_rep_1_12/Pep789101112WG.csv 8 15 16 32 | less
 perl Mayu.pl -B example.csv -C tardecdb.fa -H 41 -N 4 -O 15 -v -PmFDR -runR
+
+130603_Berit_8_AID_YFP.raw , 2759
+130603_Berit_12_AID_YFP.raw , 1393
+130603_Berit_11_AID_YFP.raw , 2026
+130603_Berit_10_AID_YFP.raw , 1997
+130603_Berit_7_AID_YFP.raw , 3034
+130603_Berit_9_AID_YFP.raw , 2042
+
+
+c=csvread('X:\Qexactive\Berit_Sissel\130521_Berit_Aid_YFP\0130603_Berit_tek_rep_1_12\scoremix.csv',0,1)
+corr(c(:,1),c(:,4))
+corr(c(:,1),c(:,5))
+corr(c(:,1),c(:,6))
+plot(c(:,2),c(:,3),'b.') % corr ~0.9
+hist(c(:,6),[100])
