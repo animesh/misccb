12faf88f3e5228ea1d1b474255bd9d80483f9c99
Ani <sharma.animesh@gmail.com>
2013-07-31 16:25:12 +0200

    regression test

--- protareacomp.m
+++ protareacomp.m
@@ -7,10 +7,6 @@ prot=prot(2:end,:)
 hr
 plot(prot(:,6),prot(:,2),'r.')
 
-%% correlation
-[rho val]=corr(prot(:,6)./prot(:,11),prot(:,8)./prot(:,11),'rows','pairwise')
-[rho val]=corr(prot(:,6),prot(:,8),'rows','pairwise')
-
 %% parameters
 for i=1:6
     i
@@ -20,3 +16,19 @@ for i=1:6
 end
 mxv,x,rho,val,hr
 plot(hr,x,'b.')
+
+
+%% error histogram
+errx=bsxfun(@minus,bsxfun(@times,prot(:,6),x),prot)
+hist(errx)
+
+%% gen prot mat
+bval=randn(100,1)
+muval=[6 5 4 3 2 1]
+prot=[bval.*6 bval.*5 bval.*4 bval.*3 bval.*2 bval.*1]
+
+
+%% correlation
+[rho val]=corr(prot(:,6)./prot(:,11),prot(:,8)./prot(:,11),'rows','pairwise')
+[rho val]=corr(prot(:,6),prot(:,8),'rows','pairwise')
+
