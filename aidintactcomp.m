33ec2c9994414b841b25fddf2b355d90da885519
Ani <sharma.animesh@gmail.com>
2013-11-15 18:16:08 +0100

    aid study area correlations

--- aidintactcomp.m
+++ aidintactcomp.m
@@ -1,5 +1,23 @@
 %% read files
-prot=xlsread('X:\Qexactive\Berit_Sissel\StimUnMC6RBR123protrep.xlsx')
+prot=xlsread('X:\Qexactive\Berit_Sissel\MCR22Proteins.xls')
+prot=prot(:,6:27)
+
+%% comp
+[pcom, z, dev] = pca1(prot')
+cumsum(dev./sum(dev) * 100)
+plot3(pcom(:,1),pcom(:,2),pcom(:,3),'r.')
+tags = num2str((1:size(pcom,1))','%d');
+text(pcom(:,1),pcom(:,2),pcom(:,3),tags)
+xlabel('PC1');
+ylabel('PC2');
+zlabel('PC3');
+title('PCA Scatter');
+
+%%cluster
+
+corrprot=corrcoef(prot,'rows','pairwise')
+ccprop=clustergram(corrprot, 'Colormap', redgreencmap(256),'ImputeFun','knnimpute')%,'Distance', 'mahalanobis')
+
 
 %% correlations
 
