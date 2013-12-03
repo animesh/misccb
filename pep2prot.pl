43fbf75101db0fd216b09cf045a81f4014828444
Ani <sharma.animesh@gmail.com>
2013-12-03 17:00:51 +0100

    match denovogui for pepnovo results to fasta

--- pep2prot.pl
+++ pep2prot.pl
@@ -1,14 +1,18 @@
 use strict;
 use warnings;
 my %seqh;
+my %sfull;
 my $seqc;
 my $f1=shift @ARGV;
 my $f2=shift @ARGV;
+my $pid=1;
+my $col=0;
+
 open(F1,$f1);
 while(my $l1=<F1>){
 	chomp $l1;
         $l1=~s/\r//g;
-        if($l1=~/^>/){my @st=split(/\|/,$l1);$seqc=$st[1];}
+        if($l1=~/^>/){my @st=split(/\|/,$l1);$seqc=$st[$pid];$sfull{$seqc}=$l1;}
         else{$l1=~s/[0-9]|\s+//g;$seqh{$seqc}.=uc($l1);}
 }
 close F1;
@@ -17,15 +21,16 @@ open(F2,$f2);
 while(my $l2=<F2>){
 	chomp $l2;
         $l2=~s/\r//g;
-        my @temp=split(/\;|\,/,$l2);
-        print "$temp[1],$temp[0],";
+        my @temp=split(/\;|\,|\t/,$l2);
         foreach my $seq (keys %seqh){
-        	if($seqh{$seq}=~/$temp[1]/){print "$seq,"}
+        	if($seqh{$seq}=~/$temp[$col]/){print "$temp[$col],$sfull{$seq},$l2\n";}
         }
-        print "\n";
+        print 
 }
 close F2;
 
 __END__
 
-perl pep2prot.pl /cygdrive/x/FastaDB/uniprot-human-oct-13.fasta /cygdrive/x/Qexactive/Mirta/QExactive/Bcell_Project/20131113_Bcell_Uracil_peptideList.txt > /cygdrive/x/Qexactive/Mirta/QExactive/Bcell_Project/20131113_Bcell_Uracil_peptideList.match.csv
+perl pep2prot.pl /cygdrive/x/FastaDB/SerpinB3P29508.fasta /cygdrive/x/Qexactive/LARS/2013/november/ole\ jan/SerpinResultsBlast.txt 2>0
+
+
