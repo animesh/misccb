858dc2dc161e29e4ad2cbf0227395ffda1c36ea7
U-WIN-NTNU-NO\animeshs <animeshs@DMED5604.klientmaskiner.medisin.ntnu.no>
2013-12-13 13:58:35 +0100

    protein cleavage and peptide counts

--- prot2pep.pl
+++ prot2pep.pl
@@ -6,11 +6,9 @@ my %seqh;
 my %sfull;
 my $seqc;
 my $f1=shift @ARGV;
-my $pid=shift @ARGV;
-#my $pid=5;
-my $col=25;
 my %pephsh;
 my %pepcnt;
+my %peplgt;
 
 open(F1,$f1);
 while(my $l1=<F1>){
@@ -22,19 +20,16 @@ while(my $l1=<F1>){
 close F1;
 
 foreach my $seq (keys %seqh){
-	my $lgt=length($seqh{$seq});	
-	for(my $c1=0;$c1<=$lgt-$pid;$c1++){
-		my $st=substr($seqh{$seq},$c1,$pid);
-		if($st=~/^[KR]/ and $st=~/[KR]$/){
-			$pephsh{$st}.="$seq;";
-			$pepcnt{$st}++;
-			#print "$seq\t$sfull{$seq}\t$seqh{$seq}\t$lgt\t$st\n";
-		}
+	my @st=split(/[KR]/,$seqh{$seq});
+	for(my $c1=0;$c1<=$#st;$c1++){
+		$peplgt{$st[$c1]}=length($st[$c1]);
+		$pephsh{$st[$c1]}.="$seq;";
+		$pepcnt{$st[$c1]}++;
 	}
 }
 
 foreach my $pep (keys %pepcnt){
-	print "$pep\t$pepcnt{$pep}\t$pephsh{$pep}\n";
+	print "$pep\t$pepcnt{$pep}\t$peplgt{$pep}\t$pephsh{$pep}\n";
 }
 
 __END__
