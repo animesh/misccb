f1502323f672da10f036a779195d6e56dbd6a494
animeshs@PROMEC <animeshs@promec01.medisin.ntnu.no>
2014-03-21 17:31:58 +0100

    tRNA map with Rsamtools, GO.db revmap, PCA with Arnar code

--- uniprot2geneanswers.r
+++ uniprot2geneanswers.r
@@ -5,8 +5,22 @@
 #biocLite("biomaRt")
 #rm(list = ls())
 data=read.csv("L:/Elite/gaute/test/SHBER.csv")
+gene=as.character(data[,1])
+data=data[,-1]
+prco=prcomp(data)
+sco <- prco$x
+loa <- prco$rotation
+plot(sco[,1], sco[,2], col=row.names(data))
+plot(loa[,1], loa[,2], col=row.names(data))
+     
+
 library(org.Hs.eg.db)
-egmap <- revmap(org.Hs.egUNIPROT)[as.character(data$Entry)]
-library('GeneAnswers')
-x <- geneAnswersBuilder(toTable(egmap), 'org.Hs.eg.db', categoryType='GO.BP', testType='hyperG',  pvalueT=0.1, FDR.correction=TRUE, geneExpressionProfile=data)
-geneAnswersReadable(x)
+egmap <- revmap(org.Hs.egUNIPROT)[as.character(gene)]
+
+x <- org.Hs.egGO
+mapped_genes <- mappedkeys(x)
+xx <- as.list(x[mapped_genes])
+goids <- xx[2:3]
+names(goids[[1]])
+
+
