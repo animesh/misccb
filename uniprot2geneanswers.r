8b6752d4c72431869b3b0d2464dd6aab7b22849f
animeshs@PROMEC <animeshs@promec01.medisin.ntnu.no>
2014-07-09 17:08:45 +0200

    fixing the gene set enrtichment analysis R code

--- uniprot2geneanswers.r
+++ uniprot2geneanswers.r
@@ -23,4 +23,45 @@ xx <- as.list(x[mapped_genes])
 goids <- xx[2:3]
 names(goids[[1]])
 
+mod <- prcomp(E)
+
+## scores (sample space)
+scores <- mod$x
+## loadings (gene space)
+loads <- mod$rotation
+
+## gene symbols
+sym <- as.character(mget(colnames(E), illuminaHumanv3SYMBOL))
+
+# plot
+pc1 = 1 # ok, checked
+pc2 = 2
+# score plot (sample space)
+x11()
+expVar <- 100 *mod$sdev / sum(mod$sdev) 
+hist(expVar)
+plot(scores[,pc1], scores[,pc2], col=cl, pch=19, main="Score-plot (Sample map)", xlab=paste("PC:", expVar[pc1]), ylab=paste("PC:", expVar[pc2]))
+text(scores[,pc1], scores[,pc2], rownames(E), pos=3)
+
+
+# loadings (gene space)
+plot.factor.name <- "lrT - T"
+col <- factor(a[,plot.factor.name])
+plot(loads[,pc1], loads[,pc2], col=col, pch='.', main="Gene weights", xlab=paste("PC:", expVar[pc1]), ylab=paste("PC:", expVar[pc2]))
+text(loads[,pc1], loads[,pc2], sym, pos=3, cex=0.4)
+
+
+
+# extract a quad 
+kk <- loads[,pc1] > 0 & loads[,pc2] > 0
+plot(kk)
+
+
+# GO mapping
+x <- org.Hs.egGO
+mapped_genes <- mappedkeys(x)
+xx <- as.list(x[mapped_genes])
+goids <- xx[2:3]
+names(goids[[1]])
+
 
