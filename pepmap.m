e7080439a3da3a092479669a9f5c43b2a7f2a380
Anix64 <animesh@fuzzylife.org>
2015-02-02 17:03:32 +0100

    match using part of ID

--- pepmap.m
+++ pepmap.m
@@ -1,20 +1,26 @@
 %% create and read file 
 %awk '{print ">Pep"NR;print $0}' peppho.txt
 %http://www.uniprot.org/uniprot/P05198
-prot=fastaread('X:\FastaDB\IFA2P05198.fasta')
-peps=fastaread('X:\Elite\Aida\SS_1\peppho.fasta')
+prot=fastaread('L:\Elite\LARS\2014\november\Lars Inger Ane Serum\kng1prot.fasta')
+peps=fastaread('L:\Elite\LARS\2014\november\Lars Inger Ane Serum\kng1pep.fasta')
 
 %% map
 plot([1 length(prot.Sequence)],[1 1],'k-')
 ylim([0 size(peps,1)+4])
 hold
+pos=zeros(size(peps,1),1);
+len=zeros(size(peps,1),1);
+id=zeros(length(prot.Sequence),1);
 for i=1:size(peps,1)
-    pos(i)=strfind(upper(prot.Sequence),upper(peps(i).Sequence));
-    len(i)=length(peps(i).Sequence);
-    plot([pos(i) pos(i)+len(i)],[i+2 i+2],'r-')
-    plot([pos(i) pos(i)+len(i)],[2 2],'b-')
-    for j=pos(i):(pos(i)+len(i)-1)
-        id(j)=1;
+    if exist(strfind(upper(prot.Sequence),upper(peps(i).Sequence)))
+        i
+        pos(i)=strfind(upper(prot.Sequence),upper(peps(i).Sequence))+0;
+        len(i)=length(peps(i).Sequence);
+        %plot([pos(i) pos(i)+len(i)],[i+2 i+2],'r-')
+        %plot([pos(i) pos(i)+len(i)],[2 2],'b-')
+        for j=pos(i):(pos(i)+len(i)-1)
+            id(j)=1;
+        end
     end
 end
 hold
